e0e56946f4f587b788b084890cac7a4491f92a64
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-25 17:49:18 -0300

    Added loading feature to the project

--- 6-Course-Project/navigation-with-event-binding/src/app/app.component.ts
+++ 6-Course-Project/navigation-with-event-binding/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  loadedFeature:string = 'recipe';
+
+  OnNavigate(feature:string){
+   this.loadedFeature = feature;
+  }
+
 }
