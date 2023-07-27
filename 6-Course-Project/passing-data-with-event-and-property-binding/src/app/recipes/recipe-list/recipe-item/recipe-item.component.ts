612a7e21cf9e5ee464783742ba7cf3f5c2c0fa4e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-27 00:58:47 -0300

    Recipes detaildone

--- 6-Course-Project/passing-data-with-event-and-property-binding/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ 6-Course-Project/passing-data-with-event-and-property-binding/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,10 +9,15 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipeElement :Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(){
+    this.recipeSelected.emit();
   }
 
 }
