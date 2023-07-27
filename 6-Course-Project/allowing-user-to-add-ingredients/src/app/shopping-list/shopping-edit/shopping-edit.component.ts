8837d9dd19ebedfb888de01662b930a4b4771aa1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-27 09:03:58 -0300

    Feature to add ings to shopping list done

--- 6-Course-Project/allowing-user-to-add-ingredients/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 6-Course-Project/allowing-user-to-add-ingredients/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('inputName', {static: true}) inputName: ElementRef;
+  @ViewChild('inputAmount', {static: true}) inputAmount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(){
+    const ingredientName = this.inputName.nativeElement.value;
+    const ingredientAmount = this.inputAmount.nativeElement.value;
+    this.ingredientAdded.emit(new Ingredient(ingredientName,ingredientAmount));
   }
 
 }
