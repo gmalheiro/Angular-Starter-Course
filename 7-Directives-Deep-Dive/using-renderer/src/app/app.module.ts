48ca13380b6f8856a85a45c6d08ca673dec34935
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-28 17:38:32 -0300

    Starting lecture about the renderer

---
+++ 7-Directives-Deep-Dive/using-renderer/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { BasicHighlight } from './basic-highlight/basic-higlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighlight
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
