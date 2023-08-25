32a77d8610b47308aaf2aaee4796f1d8d20a8c5f
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-25 19:37:11 -0300

    Lecture about cross component communication

---
+++ 9-Using-services-and-dependency-injection/using-services-for-cross-component-communication/src/app/accounts.service.ts
@@ -0,0 +1,35 @@
+//We use injectable because we are injecting a service inside this service
+import {Injectable} from '@angular/core'
+ 
+import { LoggingService } from "./logging.service";
+
+@Injectable()
+
+export class AccountsService{
+    accounts = [
+        {
+          name: 'Master Account',
+          status: 'active'
+        },
+        {
+          name: 'Testaccount',
+          status: 'inactive'
+        },
+        {
+          name: 'Hidden Account',
+          status: 'unknown'
+        }
+      ];
+    
+      constructor(private loggingService:LoggingService){}
+
+      addAccount(name:string,status:string) {
+        this.accounts.push({name:name,status:status});
+        this.loggingService.logStatusChange(status);
+      }
+    
+      updateStatus(id: number, newStatus: string) {
+        this.accounts[id].status = newStatus;
+        this.loggingService.logStatusChange(newStatus);
+      }
+    }
